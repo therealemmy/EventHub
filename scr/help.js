2adb377fc68941b807121af317fe0202d850bbb1
Moyinoluwa E. Ayeni <myemnuel@gmail.com>
2023-10-25 03:48:03 +0100

    First commit

---
+++ scr/help.js
@@ -0,0 +1,12 @@
+window.alert(
+  "Read carefully, and contact our care line if you need further help"
+);
+F;
+function myFunction() {
+  var x = document.getElementById("menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
